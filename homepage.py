7deb0f7bfdf58dc4ca68b3205c4ac93a10d24804
Prabhaash <prabhaash2005@gmail.com>
2026-04-10 12:43:57 +0530

    first commit

--- homepage.py
+++ homepage.py
@@ -1,1 +1,6 @@
 import streamlit as slt
+
+def homepage():
+    slt.title("Welcome to the Homepage!")
+    slt.write("This is the main page of our Streamlit application. Here you can find various features and information about our project.")
+    slt.write("Feel free to explore the different sections and enjoy your experience!")
